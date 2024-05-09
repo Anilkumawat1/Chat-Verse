f1cb7e11918f8e1e8b2382ff04fce28f334ac63b
Anilkumawat1 <anilkumawat31052001@gmail.com>
2024-05-09 23:57:12 +0530

    Design login signup

--- app/src/main/java/com/anilkumawat/chatverse/ui/LoginActivity.kt
+++ app/src/main/java/com/anilkumawat/chatverse/ui/LoginActivity.kt
@@ -1,16 +1,47 @@
 package com.anilkumawat.chatverse.ui
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.anilkumawat.chatverse.R
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var etEmail: EditText
+    private lateinit var etPassword: EditText
+    private lateinit var btnLogin: Button
+    private lateinit var tvSignUp: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        // Initialize
+        Initialize()
+
+        // Set click listener for the sign-up TextView
+        tvSignUp.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        btnLogin.setOnClickListener {
+
+        }
+    }
+    private fun Initialize(){
+        etEmail = findViewById(R.id.et_email)
+        etPassword = findViewById(R.id.et_password)
+        btnLogin = findViewById(R.id.btn_login)
+        tvSignUp = findViewById(R.id.tv_signup)
     }
 }
--- app/src/main/java/com/anilkumawat/chatverse/ui/SignUpActivity.kt
+++ app/src/main/java/com/anilkumawat/chatverse/ui/SignUpActivity.kt
@@ -1,6 +1,10 @@
 package com.anilkumawat.chatverse.ui
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,9 +12,28 @@ import androidx.core.view.WindowInsetsCompat
 import com.anilkumawat.chatverse.R
 
 class SignUpActivity : AppCompatActivity() {
+    private lateinit var etEmail: EditText
+    private lateinit var etPassword: EditText
+    private lateinit var etConfirmPassword: EditText
+    private lateinit var btnSignUp: Button
+    private lateinit var tvLogin: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+        Initialize()
 
+        tvLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+    private fun Initialize(){
+        etEmail = findViewById(R.id.et_email)
+        etPassword = findViewById(R.id.et_password)
+        etConfirmPassword = findViewById(R.id.et_confirm_password)
+        btnSignUp = findViewById(R.id.btn_sign_up)
+        tvLogin = findViewById(R.id.tv_login)
     }
 }
